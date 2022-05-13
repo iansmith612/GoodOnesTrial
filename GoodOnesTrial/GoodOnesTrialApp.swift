2a36eb7391457e9446c855c299dbe1ab38d3f25e
Ian Smith <iansmith612@gmail.com>
2022-05-13 16:46:10 -0500

    Initial Commit

---
+++ GoodOnesTrial/GoodOnesTrialApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoodOnesTrialApp.swift
+//  GoodOnesTrial
+//
+//  Created by Ian Smith on 5/13/22.
+//
+
+import SwiftUI
+
+@main
+struct GoodOnesTrialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
